054a57fc9121b900c1ec2b0bb94b35f8c7b60717
Reuben-Kipkemboi <reuben.kipkemboi@student.moringaschool.com>
2022-04-30 23:09:29 +0300

    ft:news class - creates a news class

---
+++ app/models/news.py
@@ -0,0 +1,11 @@
+class News:
+    
+    '''
+    Defining movie objects
+    '''
+    def __init__(self, title, image, description,author, publishedAt):
+        self.title = title
+        self.image = image
+        self.description = description
+        self.author  = author
+        self.publishedAt = publishedAt
