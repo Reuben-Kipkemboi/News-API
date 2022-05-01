30bed0b8bb1d63b93a8ee815177708798949ffb4
Reuben-Kipkemboi <reuben.kipkemboi@student.moringaschool.com>
2022-05-01 14:40:40 +0300

    ft:config-file - structures config credentials

--- config.py
+++ config.py
@@ -1,9 +1,12 @@
+import os
 class Config:
     '''
     General configuration parent class
     '''
     NEWS_API_BASE_URL ='https://newsapi.org/v2/top-headlines/{}?apiKey={}'
     SOURCE_ARTICLES_URL = 'https://newsapi.org/v2/top-headlines?sources={}&apiKey={}'
+    NEWS_API_KEY = os.environ.get('NEWS_API_KEY')
+    SECRET_KEY=os.environ.get('sundayisfun')
     #'https://newsapi.org/v2/top-headlines?sources=bbc-news&apiKey=API_KEY'
 
 
@@ -26,3 +29,8 @@ class DevConfig(Config):
     '''
 
     DEBUG = True
+    
+config_options = {
+'development':DevConfig,
+'production':ProdConfig
+}
